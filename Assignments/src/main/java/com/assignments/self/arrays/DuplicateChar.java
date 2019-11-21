fab8f67264631d58841d50f75f27ed543b5704fd
Somendu Maiti <somendu.maiti@gmail.com>
2019-11-21 22:15:26 +0100

    Few more assignments !

--- Assignments/src/main/java/com/assignments/self/arrays/DuplicateChar.java
+++ Assignments/src/main/java/com/assignments/self/arrays/DuplicateChar.java
@@ -22,8 +22,10 @@ public class DuplicateChar {
 
 	public static char getFirstNonRepeatedChar(String str) {
 		Map<Character, Integer> counts = new LinkedHashMap<>(str.length());
+
 		for (char c : str.toCharArray()) {
 			counts.put(c, counts.containsKey(c) ? counts.get(c) + 1 : 1);
+
 		}
 		for (Entry<Character, Integer> entry : counts.entrySet()) {
 			if (entry.getValue() == 1) {
