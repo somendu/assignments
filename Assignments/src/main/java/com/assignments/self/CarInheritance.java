9a18483c984c69d8a158103144c30315eb1931c6
somendu <somendu.maiti@gmail.com>
2020-03-31 11:20:42 +0530

    initial commit

---
+++ Assignments/src/main/java/com/assignments/self/CarInheritance.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.assignments.self;
+
+/**
+ * @author Admin_8_1
+ *
+ */
+public class CarInheritance {
+
+	public CarInheritance() {
+		// TODO Auto-generated constructor stub
+	}
+}
+
+abstract class Car {
+
+}
+
+class WagonR extends Car {
+	WagonR(int milage) {
+
+	}
+}
+
+class HondaCity extends Car {
+	HondaCity(int milage) {
+
+	}
+}
+
+class InnovaCrysta extends Car {
+	InnovaCrysta(int milage) {
+
+	}
+}
