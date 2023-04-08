7442669027c4057cf348412594e922df91b41e4f
Somendu <somendu.maiti@gmail.com>
2023-04-08 17:15:07 +0530

    Committing first working script

---
+++ SeleniumStart/src/test/java/com/selenium/browserstack/FirstScriptTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.selenium.browserstack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * @author somen
+ *
+ */
+public class FirstScriptTest {
+
+	@Test
+	public void eightComponents() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Work-Area\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+		String title = driver.getTitle();
+		assertEquals("Web form", title);
+
+		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
+		WebElement textBox = driver.findElement(By.name("my-text"));
+		WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+		textBox.sendKeys("Selenium");
+		submitButton.click();
+
+		WebElement message = driver.findElement(By.id("message"));
+		String value = message.getText();
+		assertEquals("Received!", value);
+
+		driver.quit();
+	}
+
+}
