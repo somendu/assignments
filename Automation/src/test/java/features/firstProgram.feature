55ca52e449bba1beb222c07f2c17b725c5514490
Somendu <somendu.maiti@gmail.com>
2023-11-23 18:53:38 +0530

    Added more changes - Parameterized Scenario

--- Automation/src/test/java/features/firstProgram.feature
+++ Automation/src/test/java/features/firstProgram.feature
@@ -1,13 +1,35 @@
- 
- 
 Feature: Application Login
 
-Scenario: Admin page default login 
+  Scenario: Admin page default login
+    Given User is on Netbanking Login Page
+    When User login into Application with "admin" and password "1234"
+    Then Home page is displayed
+    And Cards are displayed
 
-Given User is on Netbanking Login Page 
-When User login into Application 
-Then Home page is displayed 
-And Cards are displayed 
+  Scenario: User page default login
+    Given User is on Netbanking Login Page
+    When User login into Application with user1 and password 9023
+    Then Home page is displayed
+    And Cards are displayed
 
+  Scenario Outline: User page default login
+    Given User is on Netbanking Login Page
+    When User login into Application with "<Username>" and password "<Password>" 
+    Then Home page is displayed
+    And Cards are displayed
 
-   
+    Examples: 
+      | Username   | Password |
+      | debituser  | hellodbu |
+      | credituser | hellocdu |
+
+ 
+   Scenario: User page default Sign up 
+    Given User is on Practice Login Page
+    When User sign up  into Application
+    | somendu | 
+    | maiti		|
+    | sm@sm.com	|
+    |	9004030800	|
+    Then Home page is displayed
+    And Cards are displayed
--- Automation/src/test/java/stepDefinitions/MainSteps.java
+++ Automation/src/test/java/stepDefinitions/MainSteps.java
@@ -3,6 +3,8 @@
  */
 package stepDefinitions;
 
+import java.util.List;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -22,10 +24,35 @@ public class MainSteps {
 		System.out.println("User landed on Netbanking login page");
 	}
 
-	@When("User login into Application")
-	public void user_login_into_application() {
+//	@When("User login into Application with {string} and password {string}")
+//	public void user_login_into_application_with_and_password(String username, String password) {
+//		// Write code here that turns the phrase above into concrete actions
+//		System.out.println("User Login into Application with username " + username + " and password " + password);
+//	}
+
+//	@When("User login into Application with {string} and password {string} Various")
+//	public void user_login_into_application_with_and_password_various(String username, String password) {
+//		// Write code here that turns the phrase above into concrete actions
+//		System.out.println("User Login into Application with username " + username + " and password " + password);
+//	}
+
+	@Given("User is on Practice Login Page")
+	public void user_is_on_practice_login_page() {
+		System.out.println("Sign Up Page");
+	}
+
+	@When("User sign up  into Application")
+	public void user_sign_up_into_application(List<String> data) {
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+	}
+
+	@When("^User login into Application with (.+) and password (.+)$")
+	public void user_login_into_application_with_and_password_various(String username, String password) {
 		// Write code here that turns the phrase above into concrete actions
-		System.out.println("User Login into Application");
+		System.out.println("User Login into Application with username " + username + " and password " + password);
 	}
 
 	@Then("Home page is displayed")
